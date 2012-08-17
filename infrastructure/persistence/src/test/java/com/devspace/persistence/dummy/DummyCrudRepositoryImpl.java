52166e29dd2c6cff50ef0138215d26d2efa87fc8
Madhura Nishshanka <madhura@cnb27.(none)>
2012-08-17 22:53:50 +0530

    refactoring multitenancy

---
+++ infrastructure/persistence/src/test/java/com/devspace/persistence/dummy/DummyCrudRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.devspace.persistence.dummy;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author Madhura Nishshanka
+ * @since 1.0
+ */
+@Repository("dummyCrudRepository")
+public class DummyCrudRepositoryImpl extends com.devspace.persistence.repository.impl.CrudRepositoryImpl<DummyEntityBean> {
+
+    @PersistenceContext(name = "testPersistence")
+    private EntityManager entityManager;
+
+    @Override
+    public EntityManager getEntityManager() {
+
+        return entityManager;
+    }
+    public void setEntityManager(EntityManager entityManager){
+
+    }
+    @Override
+    public Class<DummyEntityBean> getClassType() {
+        return DummyEntityBean.class;
+    }
+
+}
